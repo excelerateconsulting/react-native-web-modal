6cc1b1c8528350d01cd3f4850490fcecaf333c5d
Chris Nguyen <christopher.nguyen13@gmail.com>
2020-08-26 16:10:46 +1000

    Make simple react web modal

--- examples/modal-enhance-example/src/index.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-AppRegistry.registerComponent('App', () => App);
-AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
-registerServiceWorker();
